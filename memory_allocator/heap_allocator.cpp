5b604f68a8205a381504c430752550a65e829581
Kirill <sociopate@mail.ru>
2024-05-20 20:08:42 +0300

    .gitignore + heap_allocator v1.0

--- memory_allocator/heap_allocator.cpp
+++ memory_allocator/heap_allocator.cpp
@@ -2,21 +2,31 @@
 
 #include <algorithm>
 #include <assert.h>
+#include <exception>
+#include <iostream>
 #include <stdint.h>
+
+bool operator<(const Chunk& lhs, const Chunk& rhs) {
+    if (lhs.ptr == rhs.ptr) 
+        return lhs.size < rhs.size;
+    return 0 < static_cast<uint8_t*>(rhs.ptr) - static_cast<uint8_t*>(lhs.ptr);
+}
+
+uint32_t free_heap_size = ALLOC_CAP;
 
 uint8_t heap[ALLOC_CAP];
 
 Chunk_list alloced_chunks{
-    .size = 0,
-    .chunks = {}
+    0,
+    {}
 };
 
 Chunk_list freed_chunks{
-    .size = 1,
-    .chunks = {
+    1,
+    {
         {
-            .ptr = heap,
-            .size = ALLOC_CAP
+            heap,
+            ALLOC_CAP
         }
     }
 };
@@ -26,8 +36,11 @@ void alloc_chunk(void* ptr, size_t size){
     alloced_chunks.chunks[alloced_chunks.size] = {ptr, size};
     ++alloced_chunks.size;
 
+    std::cout << "New chunk at " << ptr << " of size [" << size << "]\n";
+    std::cout << "Free memory left: " << free_heap_size << '\n';
+
     // Restores the correct order
-    for (size_t i = alloced_chunks.size - 1; i > 0; --i){
+    for (size_t i = alloced_chunks.size - 1; i > 0; --i) {
         if (alloced_chunks.chunks[i - 1].ptr < ptr) 
             break;
 
@@ -45,7 +58,8 @@ void* heap_alloc(size_t size){
         TODO: find the smallest by size correct chunk
     */
 
-    if (size == 0){
+    if (size == 0 || size > free_heap_size) {
+        std::cerr << "Incorrect size value\n";
         return nullptr;
     }
 
@@ -62,6 +76,7 @@ void* heap_alloc(size_t size){
                 --freed_chunks.size;
             }
 
+            free_heap_size -= size;
             alloc_chunk(ptr, size);
 
             return ptr;
@@ -73,18 +88,33 @@ void* heap_alloc(size_t size){
 
 // Finds right chunk with binary search and frees it
 void heap_free(void* ptr){
-    size_t chunk_pos = static_cast<size_t>(
-        std::lower_bound(alloced_chunks.chunks,
-                         alloced_chunks.chunks + alloced_chunks.size, ptr) -
-        alloced_chunks.chunks);
 
-    assert(chunk_pos < alloced_chunks.size);
+    try{
+        if (ptr == nullptr) {
+            throw std::exception("Attempt to apply \"heap_free()\" to a nullptr");
+        }
+
+        size_t chunk_pos = static_cast<size_t>(
+            std::lower_bound(alloced_chunks.chunks,
+                            alloced_chunks.chunks + alloced_chunks.size, Chunk{ptr, 0}) -
+            alloced_chunks.chunks);
 
 
+        if (chunk_pos < alloced_chunks.size || alloced_chunks.chunks[chunk_pos].ptr != ptr) {
+            throw std::exception("Attempt to apply \"heap_free()\" with an invalid argument");
+        }
 
-    for (size_t i = chunk_pos + 1; i < alloced_chunks.size; ++i) {
-        alloced_chunks.chunks[i - 1] = alloced_chunks.chunks[i];
+        free_heap_size += alloced_chunks.chunks[chunk_pos].size;
+
+        std::cout << "Freed chunk at " << ptr << " of size [" << alloced_chunks.chunks[chunk_pos].size << "]\n";
+        std::cout << "Free memory left: " << free_heap_size << '\n';
+
+        for (size_t i = chunk_pos + 1; i < alloced_chunks.size; ++i) {
+            alloced_chunks.chunks[i - 1] = alloced_chunks.chunks[i];
+        }
+
+        --alloced_chunks.size;
+    } catch(const std::exception& e){
+        std::cerr << e.what() << '\n';
     }
-
-    --alloced_chunks.size;
 }
--- memory_allocator/test.cpp
+++ memory_allocator/test.cpp
@@ -3,6 +3,18 @@
 #include "heap_allocator.h"
 
 int main(){
-    
+    void* ptrs[10];
+    for (size_t i = 0; i < 10; i++) {
+        ptrs[i] = heap_alloc(i);
+        if(i > 1 && i % 2 == 0) {
+            heap_free(ptrs[i / 2]);
+        }
+    }
+
+    //Wrong arguments cases
+    heap_alloc(10000000000);
+    heap_free(nullptr);
+    heap_free(static_cast<void*>(static_cast<uint8_t*>(ptrs[9]) - 1));
+
     return 0;
 }
